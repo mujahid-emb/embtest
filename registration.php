a204127a74c2877b2908c9371a50f1bb01ececaf
embadmin <embadmin@emb-ban-281.embitel.local>
2018-04-25 11:36:43 +0530

    test module commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Module registration handler
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Emb_Test',
+    __DIR__
+);
